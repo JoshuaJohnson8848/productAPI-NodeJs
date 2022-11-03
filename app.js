0b7b3c565ea3f5b51c2b2e41390ff5c4dffaccb9
Joshua Johnson <joshuatjohnson255@gmail.com>
2022-11-03 16:56:18 +0530

    Added CORS-Permissions in the App.js

--- app.js
+++ app.js
@@ -1,19 +1,13 @@
-const express = require('express');
-const app = express();
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-
-app.use(express.json());
-
-dotenv.config({ path: './config/.env' });
-
-mongoose
-  .connect(process.env.MONGOURI)
-  .then((result) => {
-    app.listen(process.env.PORT, (req, res, next) => {
-      console.log(`Server is Running At PORT ${process.env.PORT}`);
-    });
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'OPTIONS,GET,POST,PUT,PATCH,DELETE'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Content-Type',
+    'Authorization'
+  );
+  next();
+});
