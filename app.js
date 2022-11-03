078e5c470148b18ad6349368ed139284404b697e
Joshua Johnson <joshuatjohnson255@gmail.com>
2022-11-03 23:05:22 +0530

    Added Cors-Permission

--- app.js
+++ app.js
@@ -5,6 +5,20 @@ const dotenv = require('dotenv');
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'OPTIONS,GET,POST,PUT,PATCH,DELETE'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Content-Type',
+    'Authorization'
+  );
+  next();
+});
+
 dotenv.config({ path: './config/.env' });
 
 mongoose
