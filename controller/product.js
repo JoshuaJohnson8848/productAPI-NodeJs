c88f05eca4ae458e5a56fc7515b7bc50ebee846b
Joshua Johnson <joshuatjohnson255@gmail.com>
2022-11-04 19:15:52 +0530

    Added Controoler Function for Fetch All Products

--- controller/product.js
+++ controller/product.js
@@ -29,3 +29,25 @@ exports.addProduct = async (req, res, next) => {
     next(err);
   }
 };
+
+exports.getAll = async (req, res, next) => {
+  try {
+    const products = await Product.find();
+    if (!products) {
+      const error = new Error('Products Not Found');
+      error.status = 422;
+      throw error;
+    }
+    res
+      .status(200)
+      .json({
+        message: 'All Products Fetched Successfully',
+        product: products,
+      });
+  } catch (err) {
+    if (!err.status) {
+      err.status = 500;
+    }
+    next(err);
+  }
+};
